d76c55ca29c2c9f0b40c3ef5923bf80d437a42c6
Orestis Sin <orissin26@gmail.com>
2026-03-17 17:25:37 +0200

    Fix

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
 
-// app.use(/.*/, (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.use(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 //listen petitions
 app.listen(process.env.PORT, ()=>{
